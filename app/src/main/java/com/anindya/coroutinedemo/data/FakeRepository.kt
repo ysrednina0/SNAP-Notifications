17f7c9e5b2d8bc4b889d8267b838af650a1620c7
Nafismaulanaa <23523014@students.uii.ac.id>
2025-11-26 14:25:48 +0700

    bersihin code

--- app/src/main/java/com/anindya/coroutinedemo/data/FakeRepository.kt
+++ app/src/main/java/com/anindya/coroutinedemo/data/FakeRepository.kt
@@ -1,14 +1,3 @@
-//package com.anindya.coroutinedemo.data
-//import kotlinx.coroutines.delay
-//class FakeRepository {
-//    // suspend: can be called from a coroutine
-//    suspend fun fetchWelcomeMessage(): String {
-//        // Simulate a 2-second "network" delay
-//        delay(2000L)
-//        return "Hello from coroutine! Data loaded successfully."
-//    }
-//}
-
 package com.anindya.coroutinedemo.data
 
 import android.content.Context
