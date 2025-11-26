388221639042f17d3d7a2415f37cc9741b08f712
Nafismaulanaa <23523014@students.uii.ac.id>
2025-11-26 14:06:30 +0700

    ubah notifiaksi

--- app/src/main/java/com/anindya/coroutinedemo/ui/theme/main/MainViewModel.kt
+++ app/src/main/java/com/anindya/coroutinedemo/ui/theme/main/MainViewModel.kt
@@ -39,7 +39,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
                 NotificationHelper.showNotification(
                     getApplication(),
                     "Data Loaded",
-                    "Successfully loaded ${result.size} students."
+                    "Successfully loaded ${result.size} data."
                 )
 
             } catch (e: Exception) {
